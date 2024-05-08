7233e8da88f5b9653d705e6ea9a67a66763c6899
ChristosMantis <christosmadis@gmail.com>
2024-05-08 17:23:29 +0200

    orf.c

--- tests/code/neucleic_acids/orf_detection/orf.c
+++ tests/code/neucleic_acids/orf_detection/orf.c
@@ -2,34 +2,28 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-//#include <pthread.h>
 
 int main( int argc, char *argv[] )
 {
-char INPUT_FILE;
 
 	//Exit program if there is no input file
 
-if( argc == 1 )
+if( argc != 2 )
 		{
-        printf("Give the name of the input file\n");
-        scanf("%c", &INPUT_FILE);
+        printf("Wrong input\n\n");
+	    exit(1);
 		}
-else if (argc == 2)
-    {
-    INPUT_FILE = argv[1];
-    }
-else
-    {
-    printf("Wrong input\n\n");
-	exit(1);
-    }
 
 	//Open input file and assign pointer to it 
 	//Get the number of lines in the file
 
 FILE *fp;
-fp = fopen(INPUT_FILE, "r");
+fp* = fopen(argv[1], "r");
 
+char input;
 
+fscanf(fp, "%s", input);
+printf("%s\n", input);
+
+fclose(fp);
 }
