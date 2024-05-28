66b433e11b84c82e5445682da31de006007387a5
ChristosMantis <christosmadis@gmail.com>
2024-05-28 12:15:54 +0200

    Fixed orf_v02.c and organized files

--- tests/code/neucleic_acids/orf_detection/orf_v02.c
+++ tests/code/neucleic_acids/orf_detection/orf_v02.c
@@ -3,49 +3,10 @@
 #include <math.h>
 #include <string.h>
 
-int orf_detector(int orf_start, char* input_seq)
-
-int main( int argc, char *argv[] )
+int orf_detector(int orf_start, char* input_seq, int seq_length)
 {
 int i;
-int seq_length;
-int orf_start[10];
-int orf_finish[10];
-char input_seq[100];
-
-	//Exit program if there is no input file
-
-if( argc != 2 )
-		{
-        printf("Wrong input\n\n");
-	    exit(1);
-		}
-
-	//Open input file and assign pointer to it 
-	//Get the number of lines in the file
-
-FILE *fp;
-fp = fopen(argv[1], "r");
-
-fscanf(fp, "%s", &input_seq);
-
-seq_length = strlen(input_seq);
-
-//printf("%s\n", input_seq);			//Prints entire sequence
-//printf("%c\n", input_seq[0]);			//Prints first letter
-//printf("%d\n", seq_length);			//Prints length of sequence
-
-int start = 0;
-
-for(i=start; i<seq_length; i++)
-	{
-	if(input_seq[i] == 'A' && input_seq[i+1] == 'T' && input_seq[i+2] == 'G')
-		{
-		orf_start = i;
-		break;
-		}
-	}
-orf_finish[i] = orf_detector(orf_start, &input_seq);
+int orf_finish;	
 
 for(i=orf_start+3; i<seq_length; i+=3)
 	{
@@ -70,9 +31,60 @@ for(i=orf_start+3; i<seq_length; i+=3)
 		break;
 		}
 	}
+
+return(orf_finish);
+}
+
+
+int main( int argc, char *argv[] )
+{
+int i, j;
+int seq_length;
+int orf_start[10];
+int orf_finish[10];
+char input_seq[100];
+
+	//Exit program if there is no input file
+
+if( argc != 2 )
+		{
+        printf("Wrong input\n\n");
+	    exit(1);
+		}
+
+	//Open input file and assign pointer to it 
+	//Get the number of lines in the file
+
+FILE *fp;
+fp = fopen(argv[1], "r");
+
+fscanf(fp, "%s", &input_seq);
+
+seq_length = strlen(input_seq);
+
+int start = 0;
+j = 0;
+
+for(i=start; i<seq_length; i++)
+	{
+	if(input_seq[i] == 'A' && input_seq[i+1] == 'T' && input_seq[i+2] == 'G')
+		{
+		orf_start[j] = i;
+		j++;
+		}
+	}
+
+for(i = 0; i < j; i++)
+	{
+	orf_finish[i] = orf_detector(orf_start[i], &input_seq[0], seq_length);
+	}
+
 if(orf_finish[0] != 0)
 	{
-	printf("\nORF detected, start codon from:%d stop codon from: %d\n\n", orf_start, orf_finish);
+	for(i = 0; i < j; i++)
+		{
+		printf("\nORF detected, start codon from:%d stop codon from: %d\n\n", orf_start[i], orf_finish[i]);
+		}
 	}
 else
 	{
@@ -81,10 +93,3 @@ else
 
 fclose(fp);
 }
-
-int orf_detector(int, char*)
-	{
-	int orf_finish;
-
-	return(orf_finish);
-	}
