2c948cbab4bbcbd926b7b415f0d8c54522f5e29b
ChristosMantis <christosmadis@gmail.com>
2024-05-31 17:33:55 +0200

    Fixed orf_v04.c to work properly with all 3 reading frames

--- tests/code/neucleic_acids/orf_detection/orf_v04.c
+++ tests/code/neucleic_acids/orf_detection/orf_v04.c
@@ -39,7 +39,23 @@ void read_fasta(const char *input_file, char *input_seq)
 	fclose(fp);
 	}
 
-int orf_detector(int orf_start, char* input_seq, int seq_length)
+int orf_start_detector(int frame, char* input_seq, int seq_length, int* orf_start)
+{
+int i;
+int orf_count = 0;
+
+for(i = frame; i < seq_length; i += 3)
+	{
+	if(input_seq[i] == 'A' && input_seq[i+1] == 'T' && input_seq[i+2] == 'G')
+		{
+		orf_start[orf_count] = i;
+		orf_count++;
+		}
+	}
+return(orf_count);
+}
+
+int orf_end_detector(int orf_start, char* input_seq, int seq_length)
 {
 int i;
 int orf_finish;	
@@ -72,55 +88,31 @@ for(i=orf_start+3; i<seq_length; i+=3)
 return(orf_finish);
 }
 
-void remove_nested_orf(int *orf_start, int *orf_finish, int orf_count)
+void remove_nested_orf()
+{}
+
+void print_results(char *input_seq, int *orf_start, int *orf_finish, int *orf_count, int frame)
 {
 int i;
-const int TEMP_ORFS = 10;
-int frame_1_orf[TEMP_ORFS][2];
-int frame_2_orf[TEMP_ORFS][2];
-int frame_3_orf[TEMP_ORFS][2];
-int frame_1_count = 0;
-int frame_2_count = 0;
-int frame_3_count = 0;
 
-for(i = 0; i < orf_count; i++)
+for(i = 0; i < orf_count[0]; i++)
 	{
-	if(orf_start[1][i] == 1)
-		{
-		frame_1_orf[frame_1_count][0] = orf_start[0][i];
-		frame_1_orf[frame_1_count][1] = orf_finish[i];
-		frame_1_count++;
-		}
-	if(orf_start[1][i] == 2)
-		{
-		frame_2_orf[frame_2_count][0] = orf_start[0][i];
-		frame_2_orf[frame_2_count][1] = orf_finish[i];
-		frame_2_count++;
-		}
-	if(orf_start[1][i] == 3)
-		{
-		frame_3_orf[frame_3_count][0] = orf_start[0][i];
-		frame_3_orf[frame_3_count][1] = orf_finish[i];
-		frame_3_count++;
-		}
+	printf("\nORF detected, start codon from:%d stop codon from: %d, in frame +%d\n\n", orf_start[i], orf_finish[i], frame+1);
 	}
-
-for(i = 0; i < frame_1_count; i++)
-	{
 	
-	}
 }
+
 
 int main( int argc, char *argv[] )
 {
 const char *input_file = argv[1];
-int i;
-int orf_count;
+int i, j;
+int orf_count[3];
 int orf_frame;
 int seq_length;
-int orf_start[2][10];								//In orf_start[0][x] contains the aa where the codon starts and orf_start[1][x] contains the frame the codon is on
-int orf_finish[10];
-char input_seq[100];
+int orf_start[3][100];								//In orf_start[0][x] contains the aa for reading frame +1, orf_start[1][x] for reading frame +2 and 
+int orf_finish[3][100];								//orf_start[1][x] for reading frame +3. 
+char input_seq[1000];
 
 	//Exit program if there is no input file
 
@@ -137,45 +129,31 @@ read_fasta(input_file, &input_seq[0]);
 seq_length = strlen(input_seq);
 
 int start = 0;
-orf_count = 0;
-orf_frame = 1;
+orf_count[0] = 0;
+orf_count[1] = 0;
+orf_count[2] = 0;
 
 //Find all the start codons and the frame they are in
 
-for(i=start; i<seq_length; i++)
+for(i = 0; i < 3; i++)
 	{
-	if(input_seq[i] == 'A' && input_seq[i+1] == 'T' && input_seq[i+2] == 'G')
+	orf_count[i] = orf_start_detector(i, &input_seq[0], seq_length, &orf_start[i][0]);
+	}
+
+for(j = 0; j < 3; j++)
+	{	
+	for(i = 0; i < orf_count[j]; i++)
 		{
-		orf_start[0][orf_count] = i;
-		orf_start[1][orf_count] = orf_frame;
-		orf_count++;
-		}
-	orf_frame++;
-	if(orf_frame > 3)
-		{
-		orf_frame = 1;
+		orf_finish[j][i] = orf_end_detector(orf_start[j][i], &input_seq[0], seq_length);
 		}
 	}
 
-for(i = 0; i < orf_count; i++)
+//Print results of there are any
+for(i = 0; i < 3; i++)
 	{
-	orf_finish[i] = orf_detector(orf_start[0][i], &input_seq[0], seq_length);
-	}
-/*
-for(i = 0; i < orf_count; i++)
-	{
-	
-	}
-*/
-if(orf_finish[0] != 0)
-	{
-	for(i = 0; i < orf_count; i++)
+	if(orf_finish[i][0] != 0)
 		{
-		printf("\nORF detected, start codon from:%d stop codon from: %d, in frame +%d\n\n", orf_start[0][i], orf_finish[i], orf_start[1][i]);
+		print_results(&input_seq[0], &orf_start[i][0], &orf_finish[i][0], &orf_count[i], i);
 		}
-	}
-else
-	{
-	printf("\nNo ORFs were detected\n\n");
 	}
 }
